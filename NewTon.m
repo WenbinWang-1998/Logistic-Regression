e84cbd1758a98dcd39361bd7e379d707c72f6963
WenbinWang-1998 <74896960+WenbinWang-1998@users.noreply.github.com>
2021-04-27 21:10:30 +0800

    Update NewTon.m

--- NewTon.m
+++ NewTon.m
@@ -1,4 +1,4 @@
-function [w,b] = NewTon(X,y,iteration)
+function [w,b,los] = NewTon(X,y,iteration)
 [m,d]=size(X);
 a=ones(m,1);%撇表示转置矩阵
 X=[a X];
@@ -6,14 +6,19 @@ w=zeros(d+1,1);% starting from all zeros
 lost=L(X,y,w);%initial lost
 iter=1;
 delta=inf;
+i=1;
 while delta>0.00001&&iter<=iteration 
+    tic
     jacobian=J(X,y,w);
     hessian=H(X,y,w);
     iter=iter+1;
     w=w-pinv(hessian)*jacobian;
     newlost=L(X,y,w)
+    los(i)=newlost;
+    i=i+1;
     delta=lost-newlost;
-    lost=newlost;
+    lost=newlost;   
+    toc
 end
 b=w(1);
 w=w(2:d+1);
