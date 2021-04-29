ede18d67dd92a1cc6b56891b2ad38d05b612dce0
WenbinWang-1998 <74896960+WenbinWang-1998@users.noreply.github.com>
2021-04-29 12:01:35 +0800

    Update NewTon.m

--- NewTon.m
+++ NewTon.m
@@ -1,24 +1,22 @@
-function [w,b,los] = NewTon(X,y,iteration)
+function [w,b,los,delta] = NewTon(X,y,iteration)
 [m,d]=size(X);
 a=ones(m,1);
 X=[a X];
 w=zeros(d+1,1);% starting from all zeros
-lost=L(X,y,w);%initial lost
+loss=L(X,y,w);%initial lost
 iter=1;
 delta=inf;
 i=1;
-while delta>0.00001&&iter<=iteration 
-    tic
+while delta>0.01&&iter<=iteration
     jacobian=J(X,y,w);
     hessian=H(X,y,w);
     iter=iter+1;
     w=w-pinv(hessian)*jacobian;
-    newlost=L(X,y,w)
-    los(i)=newlost;
+    newloss=L(X,y,w);
+    los(i)=newloss;
     i=i+1;
-    delta=lost-newlost;
-    lost=newlost;   
-    toc
+    delta=loss-newloss;
+    loss=newloss;
 end
 b=w(1);
 w=w(2:d+1);
@@ -30,10 +28,10 @@ z=X*w;% m*1
 sig=1./(1+exp(1).^((-1)*z));
 end
 
-function [lost] = L(X,y,w)%X：m*d
+function [loss] = L(X,y,w)%X：m*d
 %Lost function
 sig=sigmoid(X,w);
-lost=(-1)*(sum(y.*log(sig)+(1-y).*log(1-sig)))/size(X,1);
+loss=(-1)*(sum(y.*log(sig)+(1-y).*log(1-sig)))/size(X,1);
 end
 
 function [jacobian] = J(X,y,w)
@@ -51,9 +49,3 @@ d=size(X,2);
 %hessian=(1/size(X,1))*X'*diag(newsig)*X;%d*d
 hessian=(1/size(X,1))*X'.*repmat(newsig',d,1)*X;
 end
-
-% function [hessian] = H(X,y,w)
-% %calculate Hessian matrix
-% sig=sigmoid(X,w);
-% hessian=(1/size(X,1))*(sig'*(1-sig))*(X'*X);%d*d
-% end
